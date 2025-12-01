44ff251d48973c330e158a713ee936f7ba63a264
Padinjavier <javierpadin661@mail.com>
2025-12-01 14:46:29 -0500

    inicio

---
+++ src/test/java/com/padin/finanzas/FinanzasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padin.finanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
